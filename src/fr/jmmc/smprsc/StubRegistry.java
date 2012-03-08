376ca107dd86cd96676737378124f1815d08e87e
sly74fr <slymacdev@free.fr>
2012-03-08 14:48:46 +0000

    Added stub description listing to overcome the lack of JAR introspection capabilities in JNLP deployment context.

---
+++ src/fr/jmmc/smprsc/StubRegistry.java
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.smprsc;
+
+import fr.jmmc.jmcs.jaxb.JAXBFactory;
+import fr.jmmc.jmcs.jaxb.XmlBindException;
+import fr.jmmc.jmcs.util.FileUtils;
+import fr.jmmc.smprsc.data.model.SampStubList;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * smprsc access singleton.
+ * 
+ * @author Sylvain LAFRASSE
+ */
+public class StubRegistry {
+
+    /** Logger - get from given class name */
+    private static final Logger _logger = LoggerFactory.getLogger(StubRegistry.class.getName());
+
+    /** Internal singleton instance holder */
+    private static StubRegistry _singleton = null;
+
+    /** package name for JAXB generated code */
+    private final static String SAMP_STUB_LIST_JAXB_PATH = "fr.jmmc.smprsc.data.model";
+
+    /** SAMP stub list file i.e. "index.xml" */
+    public static final String SAMP_STUB_LIST_DATA_FILE = "fr/jmmc/smprsc/registry/__index__.xml";
+
+    /** internal JAXB Factory */
+    private final JAXBFactory jf;
+
+    /**
+     * Private constructor that must be empty.
+     */
+    private StubRegistry() {
+        // Start JAXB
+        jf = JAXBFactory.getInstance(SAMP_STUB_LIST_JAXB_PATH);
+}
+
+    /**
+     * Return the singleton instance of StubRegistry.
+     *
+     * @return the singleton preference instance
+     */
+    public static StubRegistry getInstance() {
+        // Build new reference if singleton does not already exist or return previous reference
+        if (_singleton == null) {
+            _logger.debug("StubRegistry.getInstance()");
+
+            _singleton = new StubRegistry();
+        }
+
+        return _singleton;
+    }
+
+    /** Invoke JAXB to load XML file */
+    private SampStubList loadData(final URL dataModelURL) throws XmlBindException, IllegalArgumentException, IllegalStateException {
+
+        // Note : use input stream to avoid JNLP offline bug with URL (Unknown host exception)
+        try {
+            final Unmarshaller u = jf.createUnMarshaller();
+            return (SampStubList) u.unmarshal(new BufferedInputStream(dataModelURL.openStream()));
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Load failure on " + dataModelURL, ioe);
+        } catch (JAXBException je) {
+            throw new IllegalArgumentException("Load failure on " + dataModelURL, je);
+        }
+    }
+
+    /**
+     * Try to load index file content.
+     * @return the list of SAMP stub application names.
+     */
+    public List<String> getKnownApplications() {
+        final URL fileURL = FileUtils.getResource(SAMP_STUB_LIST_DATA_FILE);
+        SampStubList list = loadData(fileURL);
+        return list.getNames();
+    }
+
+    /**
+     * Print the given name list on the standard output.
+     * @param names string list to output
+     */
+    public static void printList(List<String> names) {
+        int i = 1;
+        for (String name : names) {
+            System.out.println("stub[" + i + "/" + names.size() + "] = " + name);
+            i++;
+        }
+    }
+
+    /**
+     * Main entry point
+     *
+     * @param args command line arguments (open file ...)
+     */
+    @SuppressWarnings("ResultOfObjectAllocationIgnored")
+    public static void main(final String[] args) {
+        List<String> names = StubRegistry.getInstance().getKnownApplications();
+        printList(names);
+    }
+}
+/*___oOo___*/
---
+++ src/fr/jmmc/smprsc/data/model/SampStubList.java
@@ -0,0 +1,83 @@
+
+package fr.jmmc.smprsc.data.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * 
+ *                 JMMC AppLauncher Stub list.
+ *             
+ * 
+ * <p>Java class for SampStubList complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="SampStubList">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string" maxOccurs="unbounded"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "SampStubList", propOrder = {
+    "names"
+})
+@XmlRootElement(name = "SampStubList")
+public class SampStubList {
+
+    @XmlElement(name = "name", required = true)
+    protected List<String> names;
+
+    /**
+     * Gets the value of the names property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the names property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getNames().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link String }
+     * 
+     * 
+     */
+    public List<String> getNames() {
+        if (names == null) {
+            names = new ArrayList<String>();
+        }
+        return this.names;
+    }
+
+    public boolean isSetNames() {
+        return ((this.names!= null)&&(!this.names.isEmpty()));
+    }
+
+    public void unsetNames() {
+        this.names = null;
+    }
+
+}
